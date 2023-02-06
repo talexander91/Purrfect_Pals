c8735b67ac582c9d50af4b101e5843ae6924f399
vicearus <ajvicenc@uci.edu>
2023-02-05 20:10:34 -0800

    added code to prevent crash with bad fetch request

--- client/src/pages/Fetch/catFetch.js
+++ client/src/pages/Fetch/catFetch.js
@@ -20,39 +20,54 @@ function CatFetch(props) {
       }
     );
     const data = await response.json();
+    
     setcatPic(data[0]);
     setcatDesc(data[0]);
+    console.log("hello", catPic.image_link, (catPic.image_link !== undefined))
   }
+
+
+  // useEffect(() => {
+  //   if (catPic.image_link !== undefined){
+  //     console.log(catPic)
+  //   fetchData();}
+  // }, []);
+
   useEffect(() => {
     fetchData();
-  }, []);
+  },[]);
+  
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "row",
-        width: "90%",
-        backgroundColor: "primary.dark",
-        "&:hover": {
-          backgroundColor: "primary.main",
-          opacity: [0.9, 0.8, 0.7],
-        },
-      }}
-    >
-      <CatAvatar catPic={catPic.image_link} />
-      <CatDesc
-        catDesc={[
-          catDesc.origin,
-          catDesc.name,
-          catDesc.grooming,
-          catDesc.shedding,
-          catDesc.min_life_expectancy,
-          catDesc.max_life_expectancy,
-          catDesc.min_weight,
-          catDesc.max_weight,
-        ]}
-      />
+    <Box>
+      {!catPic ? (<div></div>) : (
+        <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          width: "90%",
+          backgroundColor: "primary.dark",
+          "&:hover": {
+            backgroundColor: "primary.main",
+            opacity: [0.9, 0.8, 0.7],
+          },
+        }}>
+         <CatAvatar catPic={catPic.image_link} />
+         <CatDesc
+           catDesc={[
+             catDesc.origin,
+             catDesc.name,
+             catDesc.grooming,
+             catDesc.shedding,
+             catDesc.min_life_expectancy,
+             catDesc.max_life_expectancy,
+             catDesc.min_weight,
+             catDesc.max_weight,
+           ]}
+         />
+         </Box>
+
+      )}
     </Box>
   );
 }
