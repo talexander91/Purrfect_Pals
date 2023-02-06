c0198bf65371755d316fa35094fbd898bfdb46e3
smitbry17 <smitbry17@gmail.com>
2023-02-05 22:33:55 -0800

    routing fix v2

--- server/server.js
+++ server/server.js
@@ -23,10 +23,10 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(join(__dirname, "..", "client", "build")));
 }
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
 app.get("/", (req, res) => {
