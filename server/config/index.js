1b50430fbdedbd1f91a9a23da10bd60f7f911fd6
smitbry17 <smitbry17@gmail.com>
2023-02-05 19:54:38 -0800

    heroku 2

--- server/config/index.js
+++ server/config/index.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/exampleDB");
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/exampleDB")
+  .then(() => console.log("MongoDB has been connected"))
+  .catch((err) => console.log(err));
+
 
 module.exports = mongoose.connection;
